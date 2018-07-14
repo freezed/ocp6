e834381b743b037530fec25268d9e7b5bfa3cf7a
Fred Z <freezed@users.noreply.github.com>
2018-07-14 09:00:33 +0200

    Adds demo script showing full tables

---
+++ sql/count-id-all-tables.sql
@@ -0,0 +1,38 @@
+SELECT COUNT(id) as 'address'
+FROM `ocp6`.`address`;
+
+SELECT COUNT(id) as 'basket'
+FROM `ocp6`.`basket`;
+
+SELECT COUNT(id) as 'client'
+FROM `ocp6`.`client`;
+
+SELECT COUNT(pizza_id) as 'composition'
+FROM `ocp6`.`composition`;
+
+SELECT COUNT(id) as 'employee'
+FROM `ocp6`.`employee`;
+
+SELECT COUNT(id) as 'ingredient'
+FROM `ocp6`.`ingredient`;
+
+SELECT COUNT(id) as 'line_basket'
+FROM `ocp6`.`line_basket`;
+
+SELECT COUNT(basket_id) as 'payment_history'
+FROM `ocp6`.`payment_history`;
+
+SELECT COUNT(id) as 'payment_solution'
+FROM `ocp6`.`payment_solution`;
+
+SELECT COUNT(id) as 'phone'
+FROM `ocp6`.`phone`;
+
+SELECT COUNT(id) as 'pizza'
+FROM `ocp6`.`pizza`;
+
+SELECT COUNT(id) as 'restaurant'
+FROM `ocp6`.`restaurant`;
+
+SELECT COUNT(ingredient_id) as 'stock'
+FROM `ocp6`.`stock`;
